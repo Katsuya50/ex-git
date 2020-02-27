ed75c3631b5a66671876d5569ea61afa9ccc271b
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-27 13:45:14 +0900

    first commit

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,41 @@
+package com.example;
+
+public class Car {
+	
+	private String name;
+	private Integer speed;
+	private String color;
+	
+	public Car() {
+	}
+	
+	public Car(String name, Integer speed, String color) {
+		super();
+		this.name = name;
+		this.speed = speed;
+		this.color = color;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", speed=" + speed + ", color=" + color + "]";
+	}
+
+}
